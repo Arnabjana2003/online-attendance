1804f2eac06055fe9e94418b00d459430fb3912d
Arnab Jana <arnabjana0099@gmail.com>
2024-01-16 16:43:16 +0530

    modify

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -32,7 +32,6 @@ function Signup() {
       .then((dptcode) => {
         let dptCode = null;
         if (dptcode._snapshot.docChanges.length != 0) {
-
           dptcode.forEach((code) => {
             dptCode = code.data().departmentcode;
           });
@@ -78,42 +77,59 @@ function Signup() {
                 btn.disabled = false;
                 btn.innerText = "Signup";
               });
-          }else{
-            toast("Dopartment code is wrong. Enter the correct code")
+          } else {
+            toast("Dopartment code is wrong. Enter the correct code");
           }
-        }else{
+        } else {
           authServices
-      .signup(userData.email, userData.passcode)
-      .then((data) => {
-        services.addDocument([userData.department],{departmentcode : userData.departmentcode})
-        services.addMember({uid:data.user.uid,name:userData.name,department:userData.department,email:userData.email})
-        .then(()=>{
-          dispatch(login({uid:data.user.uid,name:userData.name,department:userData.department,email:userData.email}))
-          toast("Registration succefull");
-          name.value = "";
-          email.value = "";
-          password.value = "";
-          department.value = "";
-        })
-        .catch((err)=>{
-          authServices.deleteUser(data.user.uid).then(()=>console.log("user deleted")).catch((err)=>console.log("user deletion err: "),err)
-        })
-      })
-      .catch((err) => {
-        toast(err.message);
-      })
-      .finally(() => {
-        btn.disabled = false;
-        btn.innerText = "Signup";
-      });
+            .signup(userData.email, userData.passcode)
+            .then((data) => {
+              services.addDocument([userData.department], {
+                departmentcode: userData.departmentcode,
+              });
+              services
+                .addMember({
+                  uid: data.user.uid,
+                  name: userData.name,
+                  department: userData.department,
+                  email: userData.email,
+                })
+                .then(() => {
+                  dispatch(
+                    login({
+                      uid: data.user.uid,
+                      name: userData.name,
+                      department: userData.department,
+                      email: userData.email,
+                    })
+                  );
+                  toast("Registration succefull");
+                  name.value = "";
+                  email.value = "";
+                  password.value = "";
+                  department.value = "";
+                })
+                .catch((err) => {
+                  authServices
+                    .deleteUser(data.user.uid)
+                    .then(() => console.log("user deleted"))
+                    .catch((err) => console.log("user deletion err: "), err);
+                });
+            })
+            .catch((err) => {
+              toast(err.message);
+            })
+            .finally(() => {
+              btn.disabled = false;
+              btn.innerText = "Signup";
+            });
         }
       })
       .catch((err) => toast(err.message))
       .finally(() => {
         btn.disabled = false;
         btn.innerText = "Signup";
-      })
-      
+      });
   };
   return (
     <>
@@ -128,7 +144,17 @@ function Signup() {
           </div>
           <div className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-yellow-200">
             <h2>Signup</h2>
-            <p className="opacity-60 text-base font-normal">or<br/>Existing member ? <span onClick={()=>navigate("/login")} className=" cursor-pointer font-bold">Login here</span></p>
+            <p className="opacity-60 text-base font-normal">
+              or
+              <br />
+              New member ?{" "}
+              <span
+                onClick={() => navigate("/login")}
+                className=" cursor-pointer font-bold"
+              >
+                Login here
+              </span>
+            </p>
           </div>
         </div>
 
@@ -214,8 +240,11 @@ function Signup() {
                 >
                   <option value="">Select</option>
                   <option value="bca">BCA</option>
-                  <option value="mathematics">MATH</option>
-                  <option value="geography">GEOGRAPHY</option>
+                  <option value="computerscience">Coumputer Science</option>
+                  <option value="mathematics">Mathematics</option>
+                  <option value="physics">Physics</option>
+                  <option value="geography">Geography</option>
+                  <option value="english">English</option>
                 </select>
               </div>
             </div>
@@ -243,11 +272,23 @@ function Signup() {
               </div>
             </div>
             <div>
-              <Button type={"submit"} label={"Signup"} className="w-full bg-yellow-500 hover:bg-yellow-400 disabled:bg-yellow-200 disabled:text-slate-400" />
+              <Button
+                type={"submit"}
+                label={"Signup"}
+                className="w-full bg-yellow-500 hover:bg-yellow-400 disabled:bg-yellow-200 disabled:text-slate-400"
+              />
             </div>
           </form>
         </div>
-        <p className=" text-center text-white">Exsiting member ? <span onClick={()=>navigate("/login")} className=" cursor-pointer font-bold">Login here</span></p>
+        <p className=" text-center text-white">
+          New member ?{" "}
+          <span
+            onClick={() => navigate("/login")}
+            className=" cursor-pointer font-bold"
+          >
+            Login here
+          </span>
+        </p>
       </div>
     </>
   );
